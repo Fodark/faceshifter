3bdfed7d64aad6821f793579944ee9140bc94161
Nicola Dall'Asen <fodark@pm.me>
2021-11-29 16:20:04 +0100

    fix6

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0,1
 docker build -t dvl-faceshifter-lzanella:devel .
-docker run -it --ipc host --gpus all -v $PWD:/workspace -v /datasets/faceshifter-datasets-preprocessed:/DATA dvl-faceshifter-lzanella:devel ./run_training.sh
+docker run -it --ipc host --gpus all -v $PWD:/workspace -v ~/datasets/faceshifter-datasets-preprocessed:/DATA dvl-faceshifter-lzanella:devel ./run_training.sh
