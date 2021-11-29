28d69d45c0238ea218081c29be17f30e0bdbf416
Nicola Dall'Asen <fodark@pm.me>
2021-11-29 15:44:40 +0100

    fix

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # build docker image from Dockerfile
-docker build -t dvl-faceshifter-dlib:devel ./preprocess
+docker build -t dvl-faceshifter-dlib-lzanella:devel ./preprocess
 # run docker container from image
-docker run -it --ipc host -v $PWD/preprocess:/workspace -v /datasets/faceshifter-datasets:/DATA -v /datasets/faceshifter-datasets-preprocessed:/RESULT dvl-faceshifter-dlib:devel "python preprocess.py --root /DATA --output_dir /RESULT"
+docker run -it --ipc host -v $PWD/preprocess:/workspace -v /datasets/faceshifter-datasets:/DATA -v /datasets/faceshifter-datasets-preprocessed:/RESULT dvl-faceshifter-dlib-lzanella:devel "python preprocess.py --root /DATA --output_dir /RESULT"
