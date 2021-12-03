f4a3540aa924a62779acdb07b300ab55d994c81a
Nicola Dall'Asen <fodark@pm.me>
2021-12-03 11:04:02 +0100

    fix

--- check.sh
+++ check.sh
@@ -2,4 +2,4 @@
 # build docker image from Dockerfile
 docker build -t dvl-faceshifter-dlib-lzanella:devel ./preprocess
 # run docker container from image
-docker run -it --ipc host -v $PWD:/workspace -v ~/datasets/faceshifter-datasets:/DATA dvl-faceshifter-dlib-lzanella:devel ./run_check.sh
+docker run -it --ipc host -v $PWD:/workspace -v ~/datasets/faceshifter-datasets:/DATA dvl-faceshifter-dlib-lzanella:devel ./run_checking.sh
