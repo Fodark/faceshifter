bd545a60046d166a18e4a843ce513b5209f20c8f
Nicola Dall'Asen <fodark@pm.me>
2021-12-06 09:09:34 +0100

    fixer

--- fix_dataset.py
+++ fix_dataset.py
@@ -9,6 +9,6 @@ val_dir = os.path.join(base_dir, 'val')
 # move 99% of images from val to dir
 for root, dirs, files in os.walk(val_dir):
     for file in files:
-        if random.random() < 0.01:
+        if random.random() < 0.99:
             os.rename(os.path.join(root, file), os.path.join(train_dir, file))
 
